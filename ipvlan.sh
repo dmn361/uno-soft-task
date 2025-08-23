68ad5a76fb828162375ab543b442a069cdc08363
dmn361 <dmn4163@gmail.com>
2025-08-23 15:02:12 +0300

    fixed

--- ipvlan.sh
+++ ipvlan.sh
@@ -3,4 +3,4 @@
 sudo ip link add ipv0 link enp0s3 type ipvlan mode l2
 sudo ip addr add 192.168.1.199/24 dev ipv0
 sudo ip link set ipv0 up
-sudo ip route add 192.168.100.200/32 dev ipv0
+sudo ip route add 192.168.1.200/32 dev ipv0
